a0047ca2c366f5f6ae81e8f4e0ba6261c8196acc
est-r23 <esterrembeci06@gmail.com>
2026-04-07 11:29:33 +0200

    Update claswork.js

--- claswork.js
+++ claswork.js
@@ -1,1 +1,17 @@
+const url = 'https://twitter154.p.rapidapi.com/user/details?username=elonmusk&user_id=44196397';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '365d5fa51fmshe8349256ae1bd96p1f5f27jsne0bb6ac58e87',
+		'x-rapidapi-host': 'twitter154.p.rapidapi.com',
+		'Content-Type': 'application/json'
+	}
+};
 
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
